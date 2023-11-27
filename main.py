5cee90856fc0d6e1364fb4c63cd8dd589cdd9638
Gayoung Yang <74947395+gayoung0512@users.noreply.github.com>
2023-11-27 17:59:45 +0900

    Freezing Yolov8

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-import cv2
 from ultralytics import YOLO
+import os
+def freeze_layer(trainer):
+    model = trainer.model
+    num_freeze = 3
+    #freezing too many layers can cause the model to lose the ability to learn and make accurate predictions
+    #try freezing only a smaller number of layers: 3~5 layers
+    # retain the pre-trained weights from a previous model + fine-tune the model on a new dataset
+    # freezing initial layers: focus on learning the patterns specific to dataset B
+    print(f"Freezing {num_freeze} layers")
+    freeze = [f'model.{x}.' for x in range(num_freeze)] # layers to freeze
+    for k, v in model.named_parameters():
+        v.requires_grad = True # train all layers
+        if any(x in k for x in freeze):
+            print(f'freezing {k}')
+            v.requires_grad = False
+            print(f"{num_freeze} layers are freezed.")
+dir_path=os.getcwd()
+model = YOLO("yolov8l.pt")
+model.add_callback("on_train_start", freeze_layer)
+model.train(data=dir_path+"\data\data.yaml", epochs=100, batch=32)
 
-# Load the YOLOv8 model
-model = YOLO('yolov8l.pt')
 
-# 동영상 파일 사용시
-# video_path = "path/to/your/video/file.mp4"
-# cap = cv2.VideoCapture(video_path)
+file_list=os.listdir(dir_path)
 
-# webcam 사용시
-cap = cv2.VideoCapture(0)
-
-# Loop through the video frames
-while cap.isOpened():
-    # Read a frame from the video
-    success, frame = cap.read()
-
-    if success:
-        # Run YOLOv8 inference on the frame
-        results = model(frame,classes=0)
-
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
-
-        # Display the annotated frame
-        cv2.imshow("YOLOv8 Inference", annotated_frame)
-
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    else:
-        # Break the loop if the end of the video is reached
-        break
-
-# Release the video capture object and close the display window
-cap.release()
-cv2.destroyAllWindows()
+print(dir_path)
+print(file_list)
